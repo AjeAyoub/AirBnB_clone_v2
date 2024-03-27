393cb0157d6adb4377f65d615cd9051e7c25fdd6
AjeAyoub <ajeddabayoub.official@gmail.com>
2024-03-27 16:56:49 -0700

    script that starts a Flask web application

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,38 +1,39 @@
 #!/usr/bin/python3
-"""
-Starts a Flask web application.
-"""
+""" 3. Add third view func that redirects & has default val for variable """
 
-from flask import Flask, escape
+from flask import Flask
 
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """Display 'Hello HBNB!'"""
+@app.route('/')
+def hello_world():
+    """ Returns some text. """
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb', strict_slashes=False)
-def display_hbnb():
-    """Display 'HBNB'"""
+@app.route('/hbnb')
+def hello():
+    """ Return other text. """
     return 'HBNB'
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def display_c(text):
-    """Display 'C ', followed by the value of the text variable."""
-    return 'C {}'.format(escape(text.replace('_', ' ')))
+@app.route('/c/<text>')
+def c_text(text):
+    """ replace text with variable. """
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
 
 
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def display_python(text):
-    """Display 'Python ', followed by the value of the text variable."""
-    return 'Python {}'.format(escape(text.replace('_', ' ')))
+@app.route('/python/')
+@app.route('/python/<text>')
+def python_text(text='is cool'):
+    """ replace more text with another variable. """
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
